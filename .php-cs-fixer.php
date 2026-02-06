ad6b0a9eef093f1cc109c366e927df1735a2d3ad
Jon Cram <webignition@gmail.com>
2026-02-06 15:01:12 +0000

    Make CI builds passable (#54)

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -15,4 +15,8 @@ return $config->setRules([
     'trailing_comma_in_multiline' => false,
     'php_unit_internal_class' => false,
     'php_unit_test_class_requires_covers' => false,
+    // Following lines added to fix #55
+    // @todo remove in #46
+    'nullable_type_declaration_for_default_null_value' => false,
+    'single_line_empty_body' => false,
 ])->setFinder($finder);
